299e4a4162708290a9c43dc97102609021e2d6b8
vivektarun <vivektarun1234@gmail.com>
2025-09-04 04:32:08 +0530

    fix: update seed file

--- src/seeders/20250805191207-add-cities.js
+++ src/seeders/20250805191207-add-cities.js
@@ -3,11 +3,11 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Cities', [
-      { id: 1, name: 'New York', createdAt: new Date(), updatedAt: new Date() },
-      { id: 2, name: 'Los Angeles', createdAt: new Date(), updatedAt: new Date() },
-      { id: 3, name: 'Chicago', createdAt: new Date(), updatedAt: new Date() },
-      { id: 4, name: 'Houston', createdAt: new Date(), updatedAt: new Date() },
-      { id: 5, name: 'Phoenix', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'New York', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Los Angeles', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Chicago', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Houston', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Phoenix', createdAt: new Date(), updatedAt: new Date() },
     ]);
   },
 
